e379cdac858279981578516ea288a2cca90d1991
Jacek Chwiła <jacek.chwila@gmail.com>
2022-12-16 21:06:59 +0100

    Added simple data analysis

--- speakleash_dashboard.py
+++ speakleash_dashboard.py
@@ -7,16 +7,89 @@ from speakleash import Speakleash
 import os
 import random
 
+#Init
 
 base_dir = os.path.join(os.path.dirname(__file__))
 replicate_to = os.path.join(base_dir, "datasets")
 sl = Speakleash(replicate_to)
 
-st.set_page_config(page_title="Goodreads Analysis App", layout="wide")
+#Prepare data
 
+datasets = []
+size = []
+name = []
+category = []
+avg_doc_length = []
+avg_words_in_sentence = []
+avg_sents_in_docs = []
+avg_text_dynamics = []
+avg_nouns_to_verbs = []
+
+total_size_mb = 0
+for d in sl.datasets:
+    size_mb = round(d.characters/1024/1024)
+    datasets.append("Dataset: {0}, size: {1} MB, characters: {2}, documents: {3}".format(d.name, size_mb, d.characters, d.documents))
+    size.append(size_mb)
+    name.append(d.name)
+    category.append(d.category)
+    total_size_mb += size_mb
+    #Get metrics
+    avg_doc_length.append(d.words/d.documents)
+    try:
+      avg_words_in_sentence.append(d.words/d.sentences)
+    except:
+      avg_words_in_sentence.append(None)
+    try:
+      avg_sents_in_docs.append(d.sentences/d.documents)
+    except:
+      avg_sents_in_docs.append(None)
+    try: 
+      avg_text_dynamics.append(d.verbs/d.words)
+    except:
+      avg_text_dynamics.append(None)
+    try: 
+      avg_nouns_to_verbs.append(d.nouns/d.verbs)
+    except:
+      avg_nouns_to_verbs.append(None)
+
+data = {
+  "name": name,
+  "category": category,
+  "size": size,
+  "avg doc length": avg_doc_length,
+  "avg sentence length" : avg_words_in_sentence,
+  "avg sentences in doc": avg_sents_in_docs,
+  "avg text dynamics" : avg_text_dynamics,
+  "avg nouns to verbs" : avg_nouns_to_verbs
+}
+
+#Using name as indexer for easier navigation
+df = pd.DataFrame(data).set_index('name')
+
+#Prepare layout
+
+st.set_page_config(page_title="Speakleash Dashboard", layout="wide")
 
 row0_spacer1, row0_1, row0_spacer2, row0_2, row0_spacer3 = st.columns(
-    (0.1, 2, 0.2, 1, 0.1)
+    (0.1, 1, 0.2, 1, 0.1)
+)
+
+row1_spacer1, row1_1, row1_spacer2 = st.columns((0.1, 3.2, 0.1))
+
+row1a_space1, row1a_1, row1a_space2, row1a_2, row1a_space3 = st.columns(
+    (0.1, 1, 0.1, 1, 0.1)
+)
+
+row2_spacer1, row2_1, row2_spacer2 = st.columns((0.1, 3.2, 0.1))
+
+line1_spacer1, line1_1, line1_spacer2, line1_2, line1_spacer2= st.columns((0.1, 1, 0.1, 1, 0.1))
+
+row3_space1, row3_1, row3_space2, row3_2, row3_space3 = st.columns(
+    (0.1, 1, 0.1, 1, 0.1)
+)
+
+row4_space1, row4_1, row4_space2, row4_2, row4_space3 = st.columns(
+    (0.1, 1, 0.1, 1, 0.1)
 )
 
 row0_1.title("Speakleash a.k.a. Spichlerz Dashboard")
@@ -29,32 +102,14 @@ row0_2.subheader(
     "[WWW](https://speakleash.org/) | [GitHub](https://github.com/speakleash) | [Twitter](https://twitter.com/Speak_Leash)"
 )
 
-datasets = []
-size = []
-name = []
-category = []
 
-total_size_mb = 0
-for d in sl.datasets:
-    size_mb = round(d.characters/1024/1024)
-    datasets.append("Dataset: {0}, size: {1} MB, characters: {2}, documents: {3}".format(d.name, size_mb, d.characters, d.documents))
-    size.append(size_mb)
-    name.append(d.name)
-    category.append(d.category)
-    total_size_mb += size_mb
 
-data = {
-  "name": name,
-  "category": category,
-  "size": size,
-}
 
-df = pd.DataFrame(data)
-
-fig1a_1 = px.pie(df, values='size', names='name')
+fig1a_1 = px.pie(df, values='size', names=df.index)
 fig1a_2 = px.pie(df, values='size', names='category')
+fig2a_1 = px.bar(df, x=df.index, y='size')
 
-row1_spacer1, row1_1, row1_spacer2 = st.columns((0.1, 3.2, 0.1))
+
 
 with row1_1:
     st.markdown(
@@ -64,9 +119,6 @@ with row1_1:
     st.header('Total size: {} GB'.format(total_size_mb/1024))
 
 st.write("")
-row1a_space1, row1a_1, row1a_space2, row1a_2, row1a_space3 = st.columns(
-    (0.1, 1, 0.1, 1, 0.1)
-)
 
 with row1a_1:
     st.plotly_chart(fig1a_1, theme="streamlit", use_container_width=True)
@@ -75,26 +127,36 @@ with row1a_2:
     st.plotly_chart(fig1a_2, theme="streamlit", use_container_width=True)
 
 def submit_show(choice):
-    #Wiem brzydkie ;-) ale działa
+    #Wiem brzydkie ;-) ale działa - spoko przydatne używamy póżniej co wybrane :)
     selected = choice.split(",")[0].split(":")[1].strip()
     st.session_state['selected'] = selected
-    print(selected)
+
+
+def show_comparison(fig2a_1, filter_choice):
+    
+    fig2a_1 = px.bar(df, x=df.index, y=filter_choice)
+    
+    with row4_1:
+        st.plotly_chart(fig2a_1, theme="streamlit", use_container_width=True)
+    
+
+
+   
 
 
 choice = None
 
-row2_spacer1, row2_1, row2_spacer2 = st.columns((0.1, 3.2, 0.1))
+
 with row2_1:
     choice = st.selectbox(
         "Select one of our datasets",
-        datasets,
-    )
+        datasets, )
+    
 
     st.button('Details and stats', key='button_show',
-          on_click=submit_show, args=(choice, ))
+         on_click=submit_show, args=(choice, ))
 
 
-line1_spacer1, line1_1, line1_spacer2, line1_2, line1_spacer2= st.columns((0.1, 1, 0.1, 1, 0.1))
 
 with line1_1:
     if 'selected' in st.session_state:
@@ -124,29 +186,37 @@ with line1_2:
         st.write(meta)
 
 
+    
 st.write("")
-row3_space1, row3_1, row3_space2, row3_2, row3_space3 = st.columns(
-    (0.1, 1, 0.1, 1, 0.1)
-)
 
+
+filters = ['size','avg doc length','avg sentence length','avg sentences in doc','avg text dynamics','avg nouns to verbs']
 with row3_1:
-    if 'selected' in st.session_state:
-        st.subheader("Wykres 1")
+    filter_choice = st.selectbox(
+    "Select filter to compare",
+        filters, )
+    st.button('Update', key='comparison_show',
+         on_click=show_comparison, args=(fig2a_1, filter_choice))
 
 
 with row3_2:
     if 'selected' in st.session_state:
-        st.subheader("Wykres 1")
+        st.subheader("Selected dataset characteristics")
 
 add_vertical_space()
-row4_space1, row4_1, row4_space2, row4_2, row4_space3 = st.columns(
-    (0.1, 1, 0.1, 1, 0.1)
-)
 
-with row4_1:
-    st.subheader("TODO")
+
 
 with row4_2:
-    st.subheader("TODO")
+        if 'selected' in st.session_state:
+            theta = ['avg doc length','avg sentence length','avg sentences in doc',
+                    'avg text dynamics','avg nouns to verbs']
+            
+            #Selected dataset metrics compared to average metrics in all datasets
+            r=df[theta].loc[st.session_state['selected']]/(df[theta].sum()/len(sl.datasets))
+            radar_df = pd.DataFrame(dict(r=r, theta=theta))
+            fig = px.line_polar(radar_df, r=r, theta=theta, line_close=True)
+            st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+
 
 add_vertical_space()
