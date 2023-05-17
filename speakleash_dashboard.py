edc0fabc73a4a6d69157c71c84674837a90d9af6
jchwila <jacek.chwila@gmail.com>
2023-05-17 07:43:46 +0200

    Add files via upload

--- speakleash_dashboard.py
+++ speakleash_dashboard.py
@@ -7,11 +7,15 @@ from streamlit_extras.add_vertical_space import add_vertical_space
 from speakleash import Speakleash
 import os
 import random
+from datetime import datetime
+
 
 st.set_page_config(page_title="Speakleash Dashboard", layout="wide")
 
 @st.cache_data()
-def prepare_data():
+def prepare_data(date_string):
+
+  #Dummy datetime input string to reset cache daily. Different string triggers cache refresh
 
   base_dir = os.path.join(os.path.dirname(__file__))
   replicate_to = os.path.join(base_dir, "datasets")
@@ -27,18 +31,54 @@ def prepare_data():
   avg_text_dynamics = []
   avg_nouns_to_verbs = []
   avg_stopwords_to_words = []
-
+  total_documents = 0
+  total_characters = 0
+  total_words = 0
+  total_sentences = 0
+  total_verbs = 0
+  total_nouns = 0
+#   total_adverbs = 0
+#   total_adjectives = 0
+  total_punctuations = 0
+  total_symbols = 0
+  total_stopwords = 0
+#   total_oovs = 0
   total_size_mb = 0
 
   for d in sl.datasets:
+      punctuations = getattr(d, 'punctuations', 0)
+      symbols = getattr(d, 'symbols', 0)
+    #   oovs = getattr(d, 'oovs', 0)
       size_mb = round(d.characters/1024/1024)
       datasets.append("Dataset: {0}, size: {1} MB, characters: {2}, documents: {3}".format(d.name, size_mb, d.characters, d.documents))
       size.append(size_mb)
       name.append(d.name)
       category.append(d.category)
       total_size_mb += size_mb
-      #Get metrics
+      total_documents += getattr(d, 'documents', 0)
+      total_characters += getattr(d, 'characters', 0)
+      total_sentences += getattr(d, 'sentences', 0)
+      total_words += getattr(d, 'words', 0)
+      total_verbs += getattr(d, 'verbs', 0)
+      total_nouns += getattr(d, 'nouns', 0)
+    #   total_adverbs += getattr(d, 'adverbs', 0)
+    #   total_adjectives += getattr(d, 'adjectives', 0)
+
+      if isinstance(punctuations, list):
+        total_punctuations += len(punctuations)
+      else:
+        total_punctuations += punctuations
+      if isinstance(symbols, list):
+        total_symbols += len(symbols)
+      else:
+        total_symbols += symbols
+    #   if isinstance(oovs, list):
+    #     total_oovs += len(oovs)
+    #   else:
+    #     total_oovs += oovs
       
+      total_stopwords += getattr(d, 'stopwords', 0)
+
       try:
         avg_doc_length.append(d.words/d.documents)
       except:
@@ -64,6 +104,7 @@ def prepare_data():
       except:
         avg_stopwords_to_words.append(0)
 
+
   data = {
     "name": name,
     "category": category,
@@ -79,11 +120,11 @@ def prepare_data():
   #Using name as indexer for easier navigation
   df = pd.DataFrame(data).set_index('name')
 
-  return sl, datasets, df, total_size_mb
+  return sl, datasets, df, total_size_mb, total_documents, total_characters, total_sentences, total_words, total_verbs, total_nouns, total_punctuations, total_symbols, total_stopwords,
 
 #Init
 
-sl, datasets, df, total_size_mb = prepare_data()
+sl, datasets, df, total_size_mb, total_documents, total_characters, total_sentences, total_words, total_verbs, total_nouns, total_punctuations, total_symbols, total_stopwords = prepare_data(datetime.now().strftime("%m-%d-%Y"))
 
 
 #Prepare layout
@@ -145,9 +186,49 @@ fig2a_1 = px.bar(df, x=df.index, y='size')
 
 
 with row1_1:
+    st.markdown("""
+    <style>
+        .center {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            width: 50%;
+        }
+        .center-text {
+            text-align: center;
+        }
+        .table-responsive {
+            font-size: 0.8em;
+            margin-left: auto;
+            margin-right: auto;
+        }
+    </style>
+    """, unsafe_allow_html=True)
+    st.markdown('<div class="center-text"><h3>An open collaboration project to build a data set for Language Modeling with a capacity of at least 1TB comprised of diverse texts in Polish. Our aim is to enable machine learning research and to train a Generative Pre-trained Transformer Model from collected data</h3></div>', unsafe_allow_html=True)
+    st.markdown('<div class="center-text"><h2>Summary</h2></div>', unsafe_allow_html=True)
+    st.markdown('<div class="center-text">Join us!, we have so far:</div>', unsafe_allow_html=True)
+
+    # Same code as before to create the table
+    table_data = {
+        'Total number of documents from all sources': [total_documents],
+        'Total number of characters': [total_characters],
+        'Total number of sentences': [total_sentences],
+        'Total number of words': [total_words],
+        'Total number of verbs': [total_verbs],
+        'Total number of nouns': [total_nouns],
+        'Total number of punctuations': [total_punctuations],
+        'Total number of symbols': [total_symbols],
+        'Total number of stopwords': [total_stopwords],
+    }
+
+    df2 = pd.DataFrame(table_data)
+    
     st.markdown(
-        "An open collaboration project to build a data set for Language Modeling with a capacity of at least 1TB comprised of diverse texts in Polish. Our aim is to enable machine learning research and to train a Generative Pre-trained Transformer Model from collected data"
+        '<div class="center">' + df2.to_html(classes='table-responsive', index=False) + '</div>', 
+        unsafe_allow_html=True
     )
+
+
     
     st.plotly_chart(fig1_1, theme="streamlit", use_container_width=True)
 
@@ -181,7 +262,7 @@ with line1_1.container():
   if choice:
       st.header("Dataset: {}".format(selected_ds))
       st.write(sl.get(selected_ds).manifest)
-    
+  
 
 with line1_2:
     
@@ -204,9 +285,6 @@ with line1_2:
         st.write(txt[:200])
         st.write(meta)
 
-
- 
-
 filters = ['size','avg doc length','avg sentence length','avg sentences in doc','avg text dynamics','avg nouns to verbs','avg stopwords to words']
 with row3_1:
     filter_choice = st.selectbox(
@@ -220,8 +298,6 @@ with row3_2:
     if choice:
         st.subheader("Selected dataset characteristics")
         st.write("Metrics compared to average metrics in all datasets (average = 1)")
-
-
 
 with row4_2:
         if choice:
